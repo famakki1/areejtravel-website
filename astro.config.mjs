8cc3930573bf642b63598fcda7231af203a97865
Fraz Makki <github@fraz.makki.au>
2026-07-16 20:58:13 +0800

    Initial commit of Astro source files

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// astro.config.mjs
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  // Default: Static site generation
+  // output: "static",
+  
+  // Default: No trailing slashes
+  // trailingSlash: "ignore",
+  
+  // Default: No integrations
+  // integrations: [],
+});
